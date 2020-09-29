357efb0d343638f5780f9b87f697ced1c07ae8f1
open-schnick <jonathan.burst@gmx.de>
2020-09-29 13:12:42 +0200

    init commit

---
+++ src/main/java/de/filefighter/rest/RestApplication.java
@@ -0,0 +1,13 @@
+package de.filefighter.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/filefighter/rest/RestApplicationTests.java
@@ -0,0 +1,13 @@
+package de.filefighter.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
