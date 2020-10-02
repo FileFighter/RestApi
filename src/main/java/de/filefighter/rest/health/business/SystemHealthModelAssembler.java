4d41a7346db3159f204410b2a9d8e766bf58138a
open-schnick <jonathan.burst@gmx.de>
2020-10-02 20:33:38 +0200

    added RESTful response to SystemHealth.

--- src/main/java/de/filefighter/rest/health/business/SystemHealthModelAssembler.java
+++ src/main/java/de/filefighter/rest/health/business/SystemHealthModelAssembler.java
@@ -1,16 +1,21 @@
 package de.filefighter.rest.health.business;
 
 import de.filefighter.rest.health.data.SystemHealth;
+import de.filefighter.rest.health.rest.HealthRestController;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Component
 public class SystemHealthModelAssembler implements RepresentationModelAssembler<SystemHealth, EntityModel<SystemHealth>> {
 
     @Override
     public @NotNull EntityModel<SystemHealth> toModel(@NotNull SystemHealth entity) {
-        return EntityModel.of(entity);
+        return EntityModel.of(entity,
+                linkTo(methodOn(HealthRestController.class).getSystemHealthInfo()).withSelfRel());
     }
 }
