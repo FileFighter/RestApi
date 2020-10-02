6c79a7717029b6c0040c8f77c7a8ecf78bf5f035
open-schnick <60597856+open-schnick@users.noreply.github.com>
2020-10-02 20:30:38 +0200

    added SwaggerConfiguration

--- src/main/java/de/filefighter/rest/health/rest/HealthRestController.java
+++ src/main/java/de/filefighter/rest/health/rest/HealthRestController.java
@@ -1,12 +1,14 @@
 package de.filefighter.rest.health.rest;
 
 import de.filefighter.rest.health.data.SystemHealth;
+import io.swagger.annotations.Api;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Api(value = "System Health", tags = { "SystemHealth" })
 @RequestMapping("/health")
 public class HealthRestController {
 
