30ab3e25b3714ca0a3de15d9f082bcba642767e6
open-schnick <jonathan.burst@gmx.de>
2020-10-04 13:15:31 +0200

    FF-34 added tests pipeline

--- src/main/java/de/filefighter/rest/RestApplication.java
+++ src/main/java/de/filefighter/rest/RestApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RestApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RestApplication.class, args);
 	}
-
 }
