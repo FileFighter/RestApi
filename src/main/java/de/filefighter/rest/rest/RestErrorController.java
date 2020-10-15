900f1ad94168e7041602d707d2dae38184f8c487
open-schnick <60597856+open-schnick@users.noreply.github.com>
2020-10-15 09:15:50 +0200

    Added 404 Status to default error controller

--- src/main/java/de/filefighter/rest/rest/RestErrorController.java
+++ src/main/java/de/filefighter/rest/rest/RestErrorController.java
@@ -2,6 +2,8 @@ package de.filefighter.rest.rest;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.annotations.ApiIgnore;
@@ -13,8 +15,8 @@ public class RestErrorController implements ErrorController {
     public static final String DEFAULT_ERROR_PATH = "/error";
 
     @RequestMapping(value = DEFAULT_ERROR_PATH)
-    public EntityModel<ServerResponse> error() {
-        return new ServerResponse("denied", "This endpoint does not exist.").toModel();
+    public ResponseEntity<ServerResponse> error() {
+        return new ResponseEntity<>(new ServerResponse("denied", "This endpoint does not exist."), HttpStatus.NOT_FOUND);
     }
 
     @Override
