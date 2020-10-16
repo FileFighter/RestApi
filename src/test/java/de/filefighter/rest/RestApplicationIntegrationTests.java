5081fbe3b813829e935a8ea8f4d7ef88d4ceca54
open-schnick <jonathan.burst@gmx.de>
2020-10-16 12:20:34 +0200

    updated tests, added integration tests

---
+++ src/test/java/de/filefighter/rest/RestApplicationIntegrationTests.java
@@ -0,0 +1,20 @@
+package de.filefighter.rest;
+
+import de.filefighter.rest.health.rest.SystemHealthRestController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class RestApplicationIntegrationTests {
+
+    @Autowired
+    SystemHealthRestController controller;
+
+	@Test
+	void contextLoads() {
+	    assertThat(controller).isNotNull();
+	}
+}
