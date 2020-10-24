5eddcd89083fb930214cb85cdcad8c3188ca4571
open-schnick <jonathan.burst@gmx.de>
2020-10-24 23:42:39 +0200

    fixed wrong profile on configuration.

--- src/main/java/de/filefighter/rest/configuration/PrepareDataBaseProd.java
+++ src/main/java/de/filefighter/rest/configuration/PrepareDataBaseProd.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("dev")
+@Profile("prod")
 public class PrepareDataBaseProd {
 
     private static final Logger LOG = LoggerFactory.getLogger(PrepareDataBaseProd.class);
