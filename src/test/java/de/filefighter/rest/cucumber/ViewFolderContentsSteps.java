fb04972b5e11823f456d0bed0eb8dea3bf358a25
open-schnick <jonathan.burst@gmx.de>
2020-10-30 09:50:41 +0100

    ViewFolderContentsSteps now extends SpringIntegrationTest

--- src/test/java/de/filefighter/rest/cucumber/ViewFolderContentsSteps.java
+++ src/test/java/de/filefighter/rest/cucumber/ViewFolderContentsSteps.java
@@ -5,7 +5,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-public class ViewFolderContentsSteps {
+public class ViewFolderContentsSteps extends SpringIntegrationTest{
     @And("the folder with id {string} and path {string} exists")
     public void theFolderWithIdAndPathExists(String arg0, String arg1) {
     }
