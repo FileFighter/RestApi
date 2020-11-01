6b65e175bfcdc4158f08a203166bd907b55bc6a5
open-schnick <jonathan.burst@gmx.de>
2020-11-01 21:01:54 +0100

    added remaining BusinessServices

--- src/main/java/de/filefighter/rest/domain/token/business/AccessTokenBusinessService.java
+++ src/main/java/de/filefighter/rest/domain/token/business/AccessTokenBusinessService.java
@@ -1,5 +1,8 @@
 package de.filefighter.rest.domain.token.business;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class AccessTokenBusinessService {
     public static final long ACCESS_TOKEN_DURATION_IN_SECONDS = 3600L;
 }
