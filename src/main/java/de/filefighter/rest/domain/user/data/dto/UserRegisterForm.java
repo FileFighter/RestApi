6bc295f5b723571b76a86dcb443fb2e9cd2c8e02
Gimleux <56401687+Gimleux@users.noreply.github.com>
2020-11-08 16:17:35 +0100

    FF-87 Feature user information features (#18)
    * Added UserRegistration.feature
    
    * added UserEditInformations.feature
    
    * Added FindUser.feature
    
    * Added 'passwort in top 10k most used passwords list' case
    
    * first fixes
    
    * remaining fixes
    
    * Commented out new features to not break master.
    
    Co-authored-by: open-schnick <jonathan.burst@gmx.de>

--- src/main/java/de/filefighter/rest/domain/user/data/dto/UserRegisterForm.java
+++ src/main/java/de/filefighter/rest/domain/user/data/dto/UserRegisterForm.java
@@ -8,5 +8,6 @@ import lombok.Data;
 public class UserRegisterForm {
     private String username;
     private String password;
+    private String confirmationPassword;
     private long[] roleIds;
 }
