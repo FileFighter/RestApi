980f14e5e8b9ac06ef81c15689204d09926ff81b
open-schnick <60597856+open-schnick@users.noreply.github.com>
2020-11-17 11:40:03 +0100

    Added cors for development purposes again.

--- src/main/java/de/filefighter/rest/configuration/RestConfiguration.java
+++ src/main/java/de/filefighter/rest/configuration/RestConfiguration.java
@@ -1,5 +1,12 @@
 package de.filefighter.rest.configuration;
 
+import org.jetbrains.annotations.NotNull;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
 public class RestConfiguration {
 
     //Custom static constants
@@ -10,7 +17,15 @@ public class RestConfiguration {
     public static final String FS_PATH_HEADER = "X-FF-PATH";
     public static final String USER_BASE_URI = "/users/";
 
-    // PreventInstantiation
-    private RestConfiguration() {
+    @Bean
+    public WebMvcConfigurer configurer(){
+
+        return new WebMvcConfigurer(){
+            @Override
+            public void addCorsMappings(@NotNull CorsRegistry registry) {
+                registry.addMapping("/*")
+                        .allowedOrigins("*");
+            }
+        };
     }
 }
