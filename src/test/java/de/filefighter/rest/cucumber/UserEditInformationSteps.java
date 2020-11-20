390bcd37699650ff6d890b506a4b4c7acd591ca6
open-schnick <jonathan.burst@gmx.de>
2020-11-20 15:29:39 +0100

    updated feature files, and step def.

--- src/test/java/de/filefighter/rest/cucumber/UserEditInformationSteps.java
+++ src/test/java/de/filefighter/rest/cucumber/UserEditInformationSteps.java
@@ -11,19 +11,19 @@ import static de.filefighter.rest.TestUtils.serializeUserRequest;
 import static de.filefighter.rest.configuration.RestConfiguration.*;
 
 public class UserEditInformationSteps extends RestApplicationIntegrationTest {
-    @When("user requests change of username with value {string} and accessToken {string}")
-    public void userRequestsChangeOfUsernameWithValueAndAccessTokenAndId(String newUsername, String accessToken) {
+    @When("user requests change of username with value {string} userId {long} and accessToken {string}")
+    public void userRequestsChangeOfUsernameWithValueAndAccessTokenAndId(String newUsername, long userId, String accessToken) {
         String authHeaderString = AUTHORIZATION_BEARER_PREFIX + accessToken;
-        String url = BASE_API_URI + USER_BASE_URI + "edit";
+        String url = BASE_API_URI + USER_BASE_URI + userId + "/edit";
 
         HashMap<String, String> authHeader = new HashMap<>();
         authHeader.put("Authorization", authHeaderString);
 
-        String postBody= serializeUserRequest(null,null,null,newUsername);
-        executeRestApiCall(HttpMethod.PUT, url, authHeader,postBody);
+        String postBody = serializeUserRequest(null, null, null, newUsername);
+        executeRestApiCall(HttpMethod.PUT, url, authHeader, postBody);
     }
 
-    @When("user requests change of password with value {string} and accessToken {string} and id {string}")
+    @When("user requests change of password with value {string} userId {long} and accessToken {string}")
     public void userRequestsChangeOfPasswordWithValueAndAccessTokenAndId(String newPassword, String accessToken, String userId) {
         String authHeaderString = AUTHORIZATION_BEARER_PREFIX + accessToken;
         String url = BASE_API_URI + USER_BASE_URI + userId + "/edit";
@@ -32,9 +32,9 @@ public class UserEditInformationSteps extends RestApplicationIntegrationTest {
         authHeader.put("Authorization", authHeaderString);
 
 
-        String postBody=serializeUserRequest(newPassword,null,newPassword,null);
+        String postBody = serializeUserRequest(newPassword, null, newPassword, null);
 
 
-        executeRestApiCall(HttpMethod.GET, url, authHeader,postBody);
+        executeRestApiCall(HttpMethod.GET, url, authHeader, postBody);
     }
 }
