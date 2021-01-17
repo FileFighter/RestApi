483cd6c3dbf1395ef3a505a252dedf1d00d6f435
Open Schnick <60597856+open-schnick@users.noreply.github.com>
2021-01-17 11:34:13 +0100

    Migrated to SpringDoc (#71)
    See https://springdoc.org/index.html#migrating-from-springfox

--- src/main/java/de/filefighter/rest/domain/permission/rest/PermissionRestController.java
+++ src/main/java/de/filefighter/rest/domain/permission/rest/PermissionRestController.java
@@ -3,7 +3,7 @@ package de.filefighter.rest.domain.permission.rest;
 import de.filefighter.rest.domain.permission.data.dto.PermissionSet;
 import de.filefighter.rest.domain.permission.data.dto.request.PermissionRequest;
 import de.filefighter.rest.rest.ServerResponse;
-import io.swagger.annotations.Api;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,7 +12,7 @@ import static de.filefighter.rest.configuration.RestConfiguration.*;
 
 @Log4j2
 @RestController
-@Api(value = "Permissions Controller", tags = {"Permissions"})
+@Tag(name = "Permissions", description = "Permissions Controller")
 @RequestMapping(BASE_API_URI)
 public class PermissionRestController {
 
@@ -23,37 +23,37 @@ public class PermissionRestController {
     }
 
     //R
-    @GetMapping(FS_BASE_URI+"{fsItemId}/permission")
+    @GetMapping(FS_BASE_URI + "{fsItemId}/permission")
     public ResponseEntity<PermissionSet> getPermissionSetForFileOrFolder(
             @PathVariable long fsItemId,
             @RequestHeader(value = "Authorization", defaultValue = AUTHORIZATION_BEARER_PREFIX + "token") String accessToken
-    ){
+    ) {
 
         log.info("Requested PermissionSet for FileSystemItem {}", fsItemId);
         return permissionsRestService.getPermissionSetByIdAndToken(fsItemId, accessToken);
     }
 
-    //C U
-    @PutMapping(FS_BASE_URI+"{fsItemId}/permission")
+    //C U TODO: refactor this to be post and put.
+    @PutMapping(FS_BASE_URI + "{fsItemId}/permission")
     public ResponseEntity<ServerResponse> addUsersOrGroupsToPermissionSetForFileOrFolder(
             @PathVariable long fsItemId,
             @RequestBody PermissionRequest permissionRequest,
             @RequestHeader(value = "Authorization", defaultValue = AUTHORIZATION_BEARER_PREFIX + "token") String accessToken
-    ){
+    ) {
 
-        log.info("Requested new User or Group permissions {} for Id {}.",permissionRequest, fsItemId);
-        return permissionsRestService.addUsersOrGroupsToPermissionSetForFileOrFolderWithAccessToken(permissionRequest,fsItemId, accessToken);
+        log.info("Requested new User or Group permissions {} for Id {}.", permissionRequest, fsItemId);
+        return permissionsRestService.addUsersOrGroupsToPermissionSetForFileOrFolderWithAccessToken(permissionRequest, fsItemId, accessToken);
     }
 
     //D
-    @DeleteMapping(FS_BASE_URI+"{fsItemId}/permission")
+    @DeleteMapping(FS_BASE_URI + "{fsItemId}/permission")
     public ResponseEntity<ServerResponse> removeUsersOrGroupsFromPermissionSetForFileOrFolder(
             @PathVariable long fsItemId,
             @RequestBody PermissionRequest permissionRequest,
             @RequestHeader(value = "Authorization", defaultValue = AUTHORIZATION_BEARER_PREFIX + "token") String accessToken
-    ){
+    ) {
 
-        log.info("Requested removal of User or Group permissions {} for Id {}.",permissionRequest, fsItemId);
-        return permissionsRestService.removeUsersOrGroupsFromPermissionSetForFileOrFolderWithAccessToken(permissionRequest,fsItemId, accessToken);
+        log.info("Requested removal of User or Group permissions {} for Id {}.", permissionRequest, fsItemId);
+        return permissionsRestService.removeUsersOrGroupsFromPermissionSetForFileOrFolderWithAccessToken(permissionRequest, fsItemId, accessToken);
     }
 }
