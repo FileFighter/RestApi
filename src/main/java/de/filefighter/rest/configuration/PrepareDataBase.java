4379e99dab1a74e8a9cfc06b615069793c7807ad
open-schnick <60597856+open-schnick@users.noreply.github.com>
2021-01-22 18:27:46 +0100

    FF-279 RestApi doesn't overwrite db on restart

--- src/main/java/de/filefighter/rest/configuration/PrepareDataBase.java
+++ src/main/java/de/filefighter/rest/configuration/PrepareDataBase.java
@@ -1,5 +1,6 @@
 package de.filefighter.rest.configuration;
 
+import de.filefighter.rest.domain.common.exceptions.FileFighterDataException;
 import de.filefighter.rest.domain.filesystem.data.persistence.FileSystemEntity;
 import de.filefighter.rest.domain.filesystem.data.persistence.FileSystemRepository;
 import de.filefighter.rest.domain.token.business.AccessTokenBusinessService;
@@ -8,18 +9,25 @@ import de.filefighter.rest.domain.token.data.persistence.AccessTokenRepository;
 import de.filefighter.rest.domain.user.data.persistence.UserEntity;
 import de.filefighter.rest.domain.user.data.persistence.UserRepository;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
 
+import static de.filefighter.rest.configuration.RestConfiguration.RUNTIME_USER_ID;
 import static de.filefighter.rest.domain.filesystem.type.FileSystemType.FOLDER;
 import static de.filefighter.rest.domain.filesystem.type.FileSystemType.TEXT;
 import static de.filefighter.rest.domain.user.group.Groups.*;
 
+@SuppressWarnings({"squid:S1192", "squid:S106"})
 @Log4j2
 @Configuration
 public class PrepareDataBase {
@@ -37,72 +45,109 @@ public class PrepareDataBase {
     String date;
 
     @Bean
-    CommandLineRunner cleanDataBase(UserRepository userRepository, FileSystemRepository fileSystemRepository, AccessTokenRepository accessTokenRepository) {
+    @Profile({"dev", "prod"})
+    @Autowired
+    CommandLineRunner veryImportantFileFighterStartScript(Environment environment) {
         return args -> {
-            log.info("Starting with clean user collection.");
-            userRepository.deleteAll();
-            log.info("Starting with clean fileSystem collection.");
-            fileSystemRepository.deleteAll();
-            log.info("Starting with clean accessToken collection.");
-            accessTokenRepository.deleteAll();
+            System.out.println();
+            System.out.println("-------------------------------< REST API >-------------------------------");
+            System.out.println();
+            System.out.println("  _____   _   _          _____   _           _       _");
+            System.out.println(" |  ___| (_) | |   ___  |  ___| (_)   __ _  | |__   | |_    ___   _ __");
+            System.out.println(" | |_    | | | |  / _ \\ | |_    | |  / _  | | '_ \\  | __|  / _ \\ | '__|");
+            System.out.println(" |  _|   | | | | |  __/ |  _|   | | | (_| | | | | | | |_  |  __/ | |");
+            System.out.println(" |_|     |_| |_|  \\___| |_|     |_|  \\__, | |_| |_|  \\__|  \\___| |_|");
+            System.out.println("                                     |___/");
+            System.out.println();
+            System.out.println("Version v" + version + ", Last updated at: " + date + "");
+            System.out.println("Environment: " + Arrays.toString(environment.getActiveProfiles()));
+            System.out.println("Started: " + new Date().toString());
+            System.out.println("Running on http://localhost:" + serverPort);
+            System.out.println();
+            System.out.println("Developed by Gimleux, Valentin, Open-Schnick.");
+            System.out.println("Development Blog: https://blog.filefighter.de");
+            System.out.println("The code can be found at: https://www.github.com/filefighter");
+            System.out.println();
+            System.out.println("-------------------------------< REST API >-------------------------------");
+            System.out.println();
+
+            /*
+            System.out.println("╭---╮")
+            System.out.println("|   |")
+            System.out.println("╰---╯")
+            */
         };
     }
 
     @Bean
-    CommandLineRunner createRuntimeUser(UserRepository userRepository) {
-        return args -> log.info("Preloading system runtime user. {}", userRepository.save(UserEntity
-                .builder()
-                .userId(RestConfiguration.RUNTIME_USER_ID)
-                .username("FileFighter")
-                .lowercaseUsername("filefighter")
-                .password(null)
-                .refreshToken(null)
-                .groupIds(new long[]{SYSTEM.getGroupId()})
-                .build()));
-    }
-
-    @Bean
     @Profile("prod")
-    CommandLineRunner initDataBaseProd(UserRepository userRepository, FileSystemRepository fileSystemRepository) {
+    CommandLineRunner initDataBaseProd(UserRepository userRepository, FileSystemRepository fileSystemRepository, AccessTokenRepository accessTokenRepository) {
         return args -> {
-            log.info("Preloading default admin user: {}.", userRepository.save(UserEntity
-                    .builder()
-                    .userId(1L)
-                    .username("Admin")
-                    .lowercaseUsername("admin")
-                    .password("admin")
-                    .refreshToken("refreshToken1234")
-                    .groupIds(new long[]{FAMILY.getGroupId(), ADMIN.getGroupId()})
-                    .build()));
+            ArrayList<UserEntity> foundUsers = (ArrayList<UserEntity>) userRepository.findAll();
+            ArrayList<UserEntity> foundFileSystemEntities = (ArrayList<UserEntity>) userRepository.findAll();
+            accessTokenRepository.deleteAll(); // Cleanup purposes.
 
-            log.info("Preloading default fsStructure: {} {}.", fileSystemRepository.save(FileSystemEntity
-                            .builder()
-                            .createdByUserId(RestConfiguration.RUNTIME_USER_ID)
-                            .fileSystemId(0)
-                            .isFile(false)
-                            .path("/")
-                            .itemIds(new long[0])
-                            .lastUpdated(Instant.now().getEpochSecond())
-                            .name("HOME_Admin")
-                            .size(420)
-                            .typeId(FOLDER.getId())
-                            .visibleForGroupIds(new long[]{UNDEFINED.getGroupId(), FAMILY.getGroupId(), ADMIN.getGroupId()})
-                            .itemIds(new long[]{1})
-                            .build()),
-                    fileSystemRepository.save(FileSystemEntity.builder()
-                            .createdByUserId(1)
-                            .fileSystemId(1)
-                            .isFile(true)
-                            .lastUpdated(Instant.now().getEpochSecond())
-                            .name("dummyFile.txt")
-                            .size(420)
-                            .typeId(TEXT.getId())
-                            .editableFoGroupIds(new long[]{FAMILY.getGroupId()})
-                            .visibleForGroupIds(new long[]{FAMILY.getGroupId()})
-                            .build()));
+            if (foundUsers.isEmpty() && foundFileSystemEntities.isEmpty()) {
+                log.info("Database seems to be empty. Creating new default entities...");
+                log.info("Inserting system runtime user: {}", userRepository.save(UserEntity
+                        .builder()
+                        .userId(RUNTIME_USER_ID)
+                        .username("FileFighter")
+                        .lowercaseUsername("filefighter")
+                        .password(null)
+                        .refreshToken(null)
+                        .groupIds(new long[]{SYSTEM.getGroupId()})
+                        .build()));
+                log.info("Inserting default Admin user: {}", userRepository.save(UserEntity
+                        .builder()
+                        .userId(1)
+                        .username("Admin")
+                        .lowercaseUsername("admin")
+                        .password("admin")
+                        .refreshToken("rft1234")
+                        .groupIds(new long[]{ADMIN.getGroupId()})
+                        .build()));
+                log.info("Inserting Home directories and default structure: {} {}.", fileSystemRepository.save(FileSystemEntity
+                                .builder()
+                                .createdByUserId(RUNTIME_USER_ID)
+                                .fileSystemId(0)
+                                .isFile(false)
+                                .path("/")
+                                .itemIds(new long[0])
+                                .lastUpdated(Instant.now().getEpochSecond())
+                                .name("HOME_Admin")
+                                .size(420)
+                                .typeId(FOLDER.getId())
+                                .visibleForGroupIds(new long[]{UNDEFINED.getGroupId(), FAMILY.getGroupId(), ADMIN.getGroupId()})
+                                .itemIds(new long[]{1})
+                                .build()),
+                        fileSystemRepository.save(FileSystemEntity.builder()
+                                .createdByUserId(1)
+                                .fileSystemId(1)
+                                .isFile(true)
+                                .lastUpdated(Instant.now().getEpochSecond())
+                                .name("dummyFile.txt")
+                                .size(420)
+                                .typeId(TEXT.getId())
+                                .editableFoGroupIds(new long[]{FAMILY.getGroupId()})
+                                .visibleForGroupIds(new long[]{FAMILY.getGroupId()})
+                                .build()));
 
-            log.info("Inserting Users {}", (userRepository.findAll().size() == 1 ? MESSAGE_ON_SUCCESS : MESSAGE_ON_FAILURE));
-            log.info("Inserting fsItems {}", (fileSystemRepository.findAll().size() == 1 ? MESSAGE_ON_SUCCESS : MESSAGE_ON_FAILURE));
+                if (userRepository.findAll().size() == 2) {
+                    log.info("Inserting Users " + MESSAGE_ON_SUCCESS);
+                } else {
+                    log.error("Inserting Users " + MESSAGE_ON_FAILURE);
+                }
+
+                if (fileSystemRepository.findAll().size() == 2) {
+                    log.info("Inserting FileSystemEntities " + MESSAGE_ON_SUCCESS);
+                } else {
+                    log.error("Inserting FileSystemEntities " + MESSAGE_ON_FAILURE);
+                }
+            } else if (foundUsers.isEmpty() ^ foundFileSystemEntities.isEmpty()) {
+                // Exclusive "or".
+                throw new FileFighterDataException("The Database failed the sanity check, contact the developers or reinstall FileFighter.");
+            }
         };
     }
 
@@ -110,7 +155,24 @@ public class PrepareDataBase {
     @Profile("dev")
     CommandLineRunner initDataBaseDev(UserRepository userRepository, AccessTokenRepository accessTokenRepository, FileSystemRepository fileSystemRepository) {
         return args -> {
-            log.info("Preloading default users: {} {}.",
+            log.info("Starting with clean user collection.");
+            userRepository.deleteAll();
+            log.info("Starting with clean fileSystem collection.");
+            fileSystemRepository.deleteAll();
+            log.info("Starting with clean accessToken collection.");
+            accessTokenRepository.deleteAll();
+
+            log.info("Inserting system runtime user. {}", userRepository.save(UserEntity
+                    .builder()
+                    .userId(RUNTIME_USER_ID)
+                    .username("FileFighter")
+                    .lowercaseUsername("filefighter")
+                    .password(null)
+                    .refreshToken(null)
+                    .groupIds(new long[]{SYSTEM.getGroupId()})
+                    .build()));
+
+            log.info("Inserting default users: {} {}.",
                     userRepository.save(UserEntity
                             .builder()
                             .userId(1)
@@ -130,7 +192,7 @@ public class PrepareDataBase {
                             .groupIds(new long[]{FAMILY.getGroupId()})
                             .build()));
 
-            log.info("Preloading default tokens: {} {}",
+            log.info("Inserting default tokens: {} {}",
                     accessTokenRepository.save(AccessTokenEntity
                             .builder()
                             .userId(1)
@@ -144,9 +206,9 @@ public class PrepareDataBase {
                             .validUntil(Instant.now().getEpochSecond() + AccessTokenBusinessService.ACCESS_TOKEN_DURATION_IN_SECONDS)
                             .build()));
 
-            log.info("Preloading default fsItems: {} {} {}.",
+            log.info("Inserting default fsItems: {} {} {}.",
                     fileSystemRepository.save(FileSystemEntity.builder()
-                            .createdByUserId(RestConfiguration.RUNTIME_USER_ID)
+                            .createdByUserId(RUNTIME_USER_ID)
                             .fileSystemId(0)
                             .isFile(false)
                             .path("/")
@@ -158,7 +220,7 @@ public class PrepareDataBase {
                             .visibleForGroupIds(new long[]{FAMILY.getGroupId(), ADMIN.getGroupId()})
                             .build()),
                     fileSystemRepository.save(FileSystemEntity.builder()
-                            .createdByUserId(RestConfiguration.RUNTIME_USER_ID)
+                            .createdByUserId(RUNTIME_USER_ID)
                             .fileSystemId(1)
                             .isFile(false)
                             .path("/")
@@ -180,34 +242,22 @@ public class PrepareDataBase {
                             .visibleForGroupIds(new long[]{FAMILY.getGroupId()})
                             .build()));
 
-            log.info("Inserting FileSystemItems {}", (fileSystemRepository.findAll().size() == 3 ? MESSAGE_ON_SUCCESS : MESSAGE_ON_FAILURE));
-            log.info("Inserting token {}", (accessTokenRepository.findAll().size() == 2 ? MESSAGE_ON_SUCCESS : MESSAGE_ON_FAILURE));
-            log.info("Inserting Users {}", (userRepository.findAll().size() == 3 ? MESSAGE_ON_SUCCESS : MESSAGE_ON_FAILURE)); // because of the runtime user.
-        };
-    }
+            if (userRepository.findAll().size() == 3) {
+                log.info("Inserting Users " + MESSAGE_ON_SUCCESS);
+            } else {
+                log.error("Inserting Users " + MESSAGE_ON_FAILURE);
+            }
 
-    @Bean
-    @Profile({"dev", "prod"})
-    @SuppressWarnings("squid:S106")
-    CommandLineRunner veryImportantFileFighterStartScript() {
-        return args -> {
-            System.out.println();
-            System.out.println("-------------------------------< REST API >-------------------------------");
-            System.out.println();
-            System.out.println("  _____   _   _          _____   _           _       _");
-            System.out.println(" |  ___| (_) | |   ___  |  ___| (_)   __ _  | |__   | |_    ___   _ __");
-            System.out.println(" | |_    | | | |  / _ \\ | |_    | |  / _  | | '_ \\  | __|  / _ \\ | '__|");
-            System.out.println(" |  _|   | | | | |  __/ |  _|   | | | (_| | | | | | | |_  |  __/ | |");
-            System.out.println(" |_|     |_| |_|  \\___| |_|     |_|  \\__, | |_| |_|  \\__|  \\___| |_|");
-            System.out.println("                                     |___/");
-            System.out.println("                 Version v" + version + " Last updated at " + date + "");
-            System.out.println("               Developed by Gimleux, Valentin, Open-Schnick.");
-            System.out.println("               Development Blog: https://blog.filefighter.de");
-            System.out.println("        The code can be found at: https://www.github.com/filefighter");
-            System.out.println("                    Running on http://localhost:" + serverPort);
-            System.out.println();
-            System.out.println("-------------------------------< REST API >-------------------------------");
-            System.out.println();
+            if (fileSystemRepository.findAll().size() == 3) {
+                log.info("Inserting FileSystemEntities " + MESSAGE_ON_SUCCESS);
+            } else {
+                log.error("Inserting FileSystemEntities " + MESSAGE_ON_FAILURE);
+            }
+            if (accessTokenRepository.findAll().size() == 2) {
+                log.info("Inserting AccessToken " + MESSAGE_ON_SUCCESS);
+            } else {
+                log.error("Inserting AccessToken " + MESSAGE_ON_FAILURE);
+            }
         };
     }
 }
