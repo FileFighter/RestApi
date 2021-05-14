5ca31a0bfb7ddd556602511274b434647aa209d5
open-schnick <jonathan.burst@gmx.de>
2021-05-14 11:08:06 +0200

    Fixed IdGenerationService and added UnitTests

--- src/main/java/de/filefighter/rest/domain/filesystem/business/IdGenerationService.java
+++ src/main/java/de/filefighter/rest/domain/filesystem/business/IdGenerationService.java
@@ -1,8 +1,15 @@
 package de.filefighter.rest.domain.filesystem.business;
 
+import de.filefighter.rest.domain.common.exceptions.FileFighterDataException;
+import de.filefighter.rest.domain.filesystem.data.persistence.FileSystemEntity;
 import de.filefighter.rest.domain.filesystem.data.persistence.FileSystemRepository;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
+@Log4j2
 @Service
 public class IdGenerationService {
 
@@ -14,7 +21,19 @@ public class IdGenerationService {
     }
 
     public void initializeService() {
-        counter = fileSystemRepository.count();
+        // we could optimize the fileSystemIds by looking at the free ids between 0 and the max id.
+        List<FileSystemEntity> entityList = fileSystemRepository.findAll();
+        Optional<Long> max = entityList
+                .stream()
+                .map(FileSystemEntity::getFileSystemId)
+                .max(Long::compare);
+
+        if (max.isEmpty())
+            throw new FileFighterDataException("Couln't calculate max fileSystemId");
+
+        counter = max.get();
+        log.debug("Found {} entities in the db.", entityList.size());
+        log.debug("IdGeneration start set to {}.", counter);
     }
 
     public long peekNext() {
